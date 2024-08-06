2b4ce801f1a36cb02039e0a28b819f14e26264bf
Tasfiha Saba <77076887+tasfihasaba@users.noreply.github.com>
2024-08-06 02:22:58 -0400

    Add initial portfolio file

---
+++ tasfiha_saba/src/components/About/About.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>IT</h3>
+              <p>
+              I'm experienced in managing support tickets, optimizing device management, and creating technical documentation.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Software Developer</h3>
+              <p>
+              I have experience designing websites, optimizing infrastructure, and developing storage solutions.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Machine Learning/ AI</h3>
+              <p>
+              I have enhanced datasets, conducted exploratory analysis, and applied linear models.
+              </p>
+            </div>
+          </li>
+
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Software Engineer</h3>
+              <p>
+              I have collaborated to improve detection accuracy, developed PowerShell modules, and integrated security measures.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ tasfiha_saba/src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Tasfiha</h1>
+        <p className={styles.description}>
+        A passionate technologist with hands-on experience in IT,
+        software development, cybersecurity, and machine learning. 
+        Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:tasfiha.saba1@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
